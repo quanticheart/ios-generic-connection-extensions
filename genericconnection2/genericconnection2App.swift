058c8b5e6a500c5af0a39e52318f44e9dc60dd16
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-03-22 21:13:54 -0300

    Initial Commit

---
+++ genericconnection2/genericconnection2App.swift
@@ -0,0 +1,17 @@
+//
+//  genericconnection2App.swift
+//  genericconnection2
+//
+//  Created by Jonn Alves on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct genericconnection2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
